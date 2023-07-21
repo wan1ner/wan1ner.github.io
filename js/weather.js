dbab062974728f1ea7aaca48dfb31a157e440b79
wan1ner <1031418739@qq.com>
2023-07-21 16:15:40 +0800

    Site updated: 2023-07-21 16:15:39

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+WIDGET = {
+    "CONFIG": {
+        "modules": "012",
+        "background": "5",
+        "tmpColor": "FFFFFF",
+        "tmpSize": "18",
+        "cityColor": "FFFFFF",
+        "citySize": "18",
+        "aqiColor": "FFFFFF",
+        "aqiSize": "16",
+        "weatherIconSize": "25",
+        "alertIconSize": "18",
+        "padding": "10px 10px 10px 10px",
+        "shadow": "0",
+        "language": "auto",
+        "borderRadius": "5",
+        "fixed": "false",
+        "vertical": "top",
+        "horizontal": "left",
+        "key": "ac7113f5b9b2449680956f4806497275"
+    }
+}
